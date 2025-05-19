b72cb310d5c6a150ef481a1ed45ec3533933106b
Webdesigner Mario <82287361+M-Wiecha@users.noreply.github.com>
2025-05-19 17:58:15 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Grundlegende JavaScript-Boilerplate
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Hier kann dein JavaScript-Code platziert werden.
+    console.log('Die Seite ist vollständig geladen und bereit!');
+    
+    // Beispiel: Zugriff auf ein Element
+    // const button = document.querySelector('button');
+    // button.addEventListener('click', function() {
+    //     alert('Button wurde geklickt!');
+    // });
+});
